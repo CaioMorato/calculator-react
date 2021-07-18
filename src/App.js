71a1f3cc2667723b3faccc37194d8fb4b7ba5e0f
Caio Morato <morato_dev@protonmail.com>
2021-07-18 15:35:58 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <div></div>
+      </main>
+    );
+  }
+}
+
+export default App;
